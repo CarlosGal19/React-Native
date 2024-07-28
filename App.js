2f2f8cbd2814df18faa123620fab33180694de05
Carlos Galindo <utm22030587@utma.edu.mx>
2024-07-27 19:40:53 -0600

    chore: config expo router

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import { StatusBar } from 'expo-status-bar';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { SafeAreaProvider  } from 'react-native-safe-area-context';
 import Main from './components/Main';
 
